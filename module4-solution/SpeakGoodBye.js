daf16fdf7bee7485fc31336e35be3f0157e853dd
AavashKh <93646964+AavashKh@users.noreply.github.com>
2022-12-04 12:02:22 +0545

    Update SpeakGoodBye.js

--- module4-solution/SpeakGoodBye.js
+++ module4-solution/SpeakGoodBye.js
@@ -2,7 +2,7 @@
 
   var speakWord = "Good bye";
   var byeSpeaker = function (name) {
-    console.log(speakword + " " + name);
+    console.log(speakWord + " " + name);
   }
   window.byeSpeaker = byeSpeaker;
 })(window);
