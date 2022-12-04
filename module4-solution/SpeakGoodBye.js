afa07e4bff9cb745e0bf66a746f1245af429597f
AavashKh <93646964+AavashKh@users.noreply.github.com>
2022-12-04 12:26:12 +0545

    Add files via upload

---
+++ module4-solution/SpeakGoodBye.js
@@ -0,0 +1,8 @@
+(function (window) {
+
+  var speakword = "Good bye";
+  var byeSpeaker = function (name) {
+    console.log(speakword + " " + name);
+  }
+  window.byeSpeaker = byeSpeaker;
+})(window);
