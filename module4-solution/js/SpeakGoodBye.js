388186f27cb1f586fdf89aeef6c669f03d9fa8a3
Aavash Kharel <aavash869@gmail.com>
2022-12-04 12:05:35 +0545

    commit

--- module4-solution/js/SpeakGoodBye.js
+++
@@ -1,7 +0,0 @@
-(function (window) {
-  var speakWord = "Good bye";
-  var byeSpeaker = function (name) {
-    console.log(speakWord + " " + name);
-  }
-  window.byeSpeaker = byeSpeaker;
-})(window);
